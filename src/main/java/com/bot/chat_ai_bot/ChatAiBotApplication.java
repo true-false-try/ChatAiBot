f97ece96fe5fdb89e8f084a098e81b81ba84ad0c
true-false-try <6mateo6ch@gmail.com>
2026-01-12 18:56:24 +0200

    [BOT_2] - added redis, need changed prompt serviceused redis

--- src/main/java/com/bot/chat_ai_bot/ChatAiBotApplication.java
+++ src/main/java/com/bot/chat_ai_bot/ChatAiBotApplication.java
@@ -2,8 +2,10 @@ package com.bot.chat_ai_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class ChatAiBotApplication {
 
     public static void main(String[] args) {
