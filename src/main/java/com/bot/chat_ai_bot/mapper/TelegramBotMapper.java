28cd863e89cf629350d4442f9ce5927547191f32
true-false-try <6mateo6ch@gmail.com>
2026-02-13 22:19:02 +0200

    [BOT_3] - added sticker deleted part? but need 10 massegs save to context and added ollama for save from exception

--- src/main/java/com/bot/chat_ai_bot/mapper/TelegramBotMapper.java
+++ src/main/java/com/bot/chat_ai_bot/mapper/TelegramBotMapper.java
@@ -1,7 +1,6 @@
 package com.bot.chat_ai_bot.mapper;
 
 import com.bot.chat_ai_bot.dto.UserDto;
-import lombok.NonNull;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
@@ -9,5 +8,5 @@ import java.math.BigInteger;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TelegramBotMapper {
-    UserDto toUserDto(BigInteger userId, String firstName, String lastName, String username, Long createdAt, String languageCode, String chatId);
+    UserDto toUserDto(BigInteger userId, String firstName, String lastName, String userName, Long createdAt, String languageCode, String chatId);
 }
