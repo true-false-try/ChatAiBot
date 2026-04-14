054fccbb5c4608a4f8ff290eafda71926a0c0721
true-false-try <6mateo6ch@gmail.com>
2026-04-14 18:33:47 +0300

    [BOT_3] -  simplify analyse mood

--- src/main/java/com/bot/chat_ai_bot/repository/SessionRepository.java
+++ src/main/java/com/bot/chat_ai_bot/repository/SessionRepository.java
@@ -1,7 +1,6 @@
 package com.bot.chat_ai_bot.repository;
 
 import com.bot.chat_ai_bot.entity.SessionEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/bot/chat_ai_bot/service/ai/impl/OllamaAnalysisServiceImpl.java
+++ src/main/java/com/bot/chat_ai_bot/service/ai/impl/OllamaAnalysisServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -27,12 +26,12 @@ public class OllamaAnalysisServiceImpl implements OllamaAnalysisService {
 
     @Override
     public Mood analyzeMood(List<MoodTaskDto> batch) {
-        String conversationHistory = batch.stream()
+        String conversationHistory = String.valueOf(batch.stream()
                 .map(task ->
-                        "USER request --> " + task.userRequest() +
+                        "USER request: " + task.userRequest() +
                         " " +
-                        "AI response --> " + task.aiResponse())
-                .collect(Collectors.joining("\n"));
+                        "AI response: " + task.aiResponse())
+                .toList());
 
         PromptTemplate template = getPromptTemplate();
         Prompt prompt = template.create(Map.of(
@@ -48,27 +47,22 @@ public class OllamaAnalysisServiceImpl implements OllamaAnalysisService {
             var response = analyticsModel.call(prompt);
             MoodResponse result = outputConverter.convert(response.getResult().getOutput().getContent());
             return result != null ? result.detectedMood() : Mood.UNKNOWN;
-        } catch (Exception e) {
+        } catch (Exception ex) {
             return Mood.UNKNOWN;
         }
     }
 
     private static @NonNull PromptTemplate getPromptTemplate() {
-        String userPrompt = """
-                            Analyze the following user messages and determine their overall emotional state (Mood).
-                            
-                            You MUST choose exactly one value from the following list: {allowed_moods}.
-                            
-                            User messages to analyze:
-                            {history}
-                            
-                            {format_instructions}
-                            """;
-
-        PromptTemplate template = new PromptTemplate(userPrompt);
-        return template;
+        return new PromptTemplate(
+                """
+                 Analyze the following user messages and determine their overall emotional state (Mood).
+                 You MUST choose exactly one value from the following list: {allowed_moods}.
+                 User messages to analyze:
+                 {history}
+                 {format_instructions}
+                 """
+        );
     }
-
 
     public record MoodResponse(Mood detectedMood) {}
 
